1df2cc78419166fdc73cce185673f93e356613ac
Kevin Campos <kevin.campos@nkey.com.br>
2016-07-04 00:33:26 -0300

    Create dummy data and list it

--- app/src/main/java/br/com/memorify/sunshine/MainActivity.java
+++ app/src/main/java/br/com/memorify/sunshine/MainActivity.java
@@ -1,7 +1,9 @@
 package br.com.memorify.sunshine;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] weekForecast = {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+
+        ArrayAdapter<String> forecastAdapter = new ArrayAdapter<>(
+        getBaseContext(), // The current context (this activity)
+        R.layout.list_item_forecast, // The name of the layout ID.
+        R.id.list_item_forecast_textview, // The ID of the textview to populate.
+                    weekForecast);
+
+        ListView forecastListView = (ListView) findViewById(R.id.listview_forecast);
+        forecastListView.setAdapter(forecastAdapter);
     }
 }
