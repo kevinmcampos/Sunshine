808f35c31c41cc0384f5ef23c97b972cdae04139
Kevin Campos <kevin.campos@nkey.com.br>
2016-07-05 22:06:47 -0300

    Fetch weather data using Open Weather Map API

--- app/src/main/java/br/com/memorify/sunshine/MainActivity.java
+++ app/src/main/java/br/com/memorify/sunshine/MainActivity.java
@@ -2,33 +2,83 @@ package br.com.memorify.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<String> weekForecast;
+    private ArrayAdapter<String> forecastAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] weekForecast = {
-                "Mon 6/23 - Sunny - 31/17",
+        String[] dummyData = { "Mon 6/23 - Sunny - 31/17",
                 "Tue 6/24 - Foggy - 21/8",
                 "Wed 6/25 - Cloudy - 22/17",
                 "Thurs 6/26 - Rainy - 18/11",
                 "Fri 6/27 - Foggy - 21/10",
                 "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                "Sun 6/29 - Sunny - 20/7"
-        };
+                "Sun 6/29 - Sunny - 20/7" };
+        weekForecast = new ArrayList<>(Arrays.asList(dummyData));
 
-        ArrayAdapter<String> forecastAdapter = new ArrayAdapter<>(
-        getBaseContext(), // The current context (this activity)
-        R.layout.list_item_forecast, // The name of the layout ID.
-        R.id.list_item_forecast_textview, // The ID of the textview to populate.
-                    weekForecast);
+        forecastAdapter = new ArrayAdapter<>(
+                getBaseContext(), // The current context (this activity)
+                R.layout.list_item_forecast, // The name of the layout ID.
+                R.id.list_item_forecast_textview, // The ID of the textview to populate.
+                weekForecast);
+        forecastAdapter.setNotifyOnChange(false);
 
         ListView forecastListView = (ListView) findViewById(R.id.listview_forecast);
         forecastListView.setAdapter(forecastAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.refresh_action:
+                fetchWeather();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void fetchWeather() {
+        final String MOUNTAIN_VIEW_LOCATION_QUERY = "94043";
+        new FetchWeatherTask(MOUNTAIN_VIEW_LOCATION_QUERY, new FetchWeatherTask.FetchWeatherCallbacks() {
+            @Override
+            public void onSuccess(String[] weatherForecasts) {
+                weekForecast = Arrays.asList(weatherForecasts);
+
+                forecastAdapter.clear();
+                for (String weekString : weekForecast) {
+                    forecastAdapter.add(weekString);
+                }
+                forecastAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailed() {
+                Toast.makeText(getBaseContext(), "Failed to fetch weather data.", Toast.LENGTH_SHORT).show();
+            }
+        }).execute();
     }
 }
