23396676c7bbefd19369798a4b0eef3565ad7a9c
Kevin Campos <kevinmcampos@gmail.com>
2016-07-09 22:08:10 -0300

    Share a forecast text using ShareActionProvider

--- app/src/main/java/br/com/memorify/sunshine/DetailActivity.java
+++ app/src/main/java/br/com/memorify/sunshine/DetailActivity.java
@@ -2,7 +2,11 @@ package br.com.memorify.sunshine;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -17,5 +21,25 @@ public class DetailActivity extends AppCompatActivity {
         String forecast = getIntent().getStringExtra(Intent.EXTRA_TEXT);
         TextView detailTextView = (TextView) findViewById(R.id.detail_text);
         detailTextView.setText(forecast);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail_menu, menu);
+        MenuItem item =  menu.findItem(R.id.share_action);
+        ShareActionProvider shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        if (shareActionProvider != null) {
+            shareActionProvider.setShareIntent(createShareForecastIntent());
+        }
+        return true;
+    }
+
+    private Intent createShareForecastIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        String forecast = getIntent().getStringExtra(Intent.EXTRA_TEXT);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, forecast + " #SunshineApp");
+        return shareIntent;
     }
 }
